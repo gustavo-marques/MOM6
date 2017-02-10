648c43f54b105993d07411cc986f06f83e414e82
Gustavo Marques <gmarques@princeton.edu>
2017-02-10 18:03:32 -0500

    Adds passive tracer and sponge layer

--- src/tracer/MOM_tracer_flow_control.F90
+++ src/tracer/MOM_tracer_flow_control.F90
@@ -55,6 +55,9 @@ use DOME_tracer, only : DOME_tracer_end, DOME_tracer_CS
 use ISOMIP_tracer, only : register_ISOMIP_tracer, initialize_ISOMIP_tracer
 use ISOMIP_tracer, only : ISOMIP_tracer_column_physics, ISOMIP_tracer_surface_state
 use ISOMIP_tracer, only : ISOMIP_tracer_end, ISOMIP_tracer_CS
+use OUT_IS_tracer, only : register_OUT_IS_tracer, initialize_OUT_IS_tracer
+use OUT_IS_tracer, only : OUT_IS_tracer_column_physics, OUT_IS_tracer_surface_state
+use OUT_IS_tracer, only : OUT_IS_tracer_end, OUT_IS_tracer_CS
 use ideal_age_example, only : register_ideal_age_tracer, initialize_ideal_age_tracer
 use ideal_age_example, only : ideal_age_tracer_column_physics, ideal_age_tracer_surface_state
 use ideal_age_example, only : ideal_age_stock, ideal_age_example_end, ideal_age_tracer_CS
@@ -94,6 +97,7 @@ type, public :: tracer_flow_control_CS ; private
   logical :: use_USER_tracer_example = .false.
   logical :: use_DOME_tracer = .false.
   logical :: use_ISOMIP_tracer = .false.
+  logical :: use_OUT_IS_tracer = .false.
   logical :: use_ideal_age = .false.
   logical :: use_regional_dyes = .false.
   logical :: use_oil = .false.
@@ -105,6 +109,7 @@ type, public :: tracer_flow_control_CS ; private
   type(USER_tracer_example_CS), pointer :: USER_tracer_example_CSp => NULL()
   type(DOME_tracer_CS), pointer :: DOME_tracer_CSp => NULL()
   type(ISOMIP_tracer_CS), pointer :: ISOMIP_tracer_CSp => NULL()
+  type(OUT_IS_tracer_CS), pointer :: OUT_IS_tracer_CSp => NULL()
   type(ideal_age_tracer_CS), pointer :: ideal_age_tracer_CSp => NULL()
   type(dye_tracer_CS), pointer :: dye_tracer_CSp => NULL()
   type(oil_tracer_CS), pointer :: oil_tracer_CSp => NULL()
@@ -162,6 +167,9 @@ subroutine call_tracer_register(HI, GV, param_file, CS, tr_Reg, restart_CS)
   call get_param(param_file, mod, "USE_ISOMIP_TRACER", CS%use_ISOMIP_tracer, &
                  "If true, use the ISOMIP_tracer tracer package.", &
                  default=.false.)
+  call get_param(param_file, mod, "USE_OUT_IS_TRACER", CS%use_OUT_IS_tracer, &
+                 "If true, use the OUT_IS_tracer tracer package.", &
+                 default=.false.)
   call get_param(param_file, mod, "USE_IDEAL_AGE_TRACER", CS%use_ideal_age, &
                  "If true, use the ideal_age_example tracer package.", &
                  default=.false.)
@@ -205,6 +213,9 @@ subroutine call_tracer_register(HI, GV, param_file, CS, tr_Reg, restart_CS)
                          tr_Reg, restart_CS)
   if (CS%use_ISOMIP_tracer) CS%use_ISOMIP_tracer = &
     register_ISOMIP_tracer(HI, GV, param_file, CS%ISOMIP_tracer_CSp, &
+                           tr_Reg, restart_CS)
+  if (CS%use_OUT_IS_tracer) CS%use_OUT_IS_tracer = &
+    register_OUT_IS_tracer(HI, GV, param_file, CS%OUT_IS_tracer_CSp, &
                            tr_Reg, restart_CS)
   if (CS%use_ideal_age) CS%use_ideal_age = &
     register_ideal_age_tracer(HI, GV, param_file,  CS%ideal_age_tracer_CSp, &
@@ -283,6 +294,9 @@ subroutine tracer_flow_control_init(restart, day, G, GV, h, param_file, diag, OB
   if (CS%use_ISOMIP_tracer) &
     call initialize_ISOMIP_tracer(restart, day, G, GV, h, diag, OBC, CS%ISOMIP_tracer_CSp, &
                                 ALE_sponge_CSp, diag_to_Z_CSp)
+  if (CS%use_OUT_IS_tracer) &
+    call initialize_OUT_IS_tracer(restart, day, G, GV, h, diag, OBC, CS%OUT_IS_tracer_CSp, &
+                                sponge_CSp, diag_to_Z_CSp)
   if (CS%use_ideal_age) &
     call initialize_ideal_age_tracer(restart, day, G, GV, h, diag, OBC, CS%ideal_age_tracer_CSp, &
                                      sponge_CSp, diag_to_Z_CSp)
@@ -428,6 +442,11 @@ subroutine call_tracer_column_fns(h_old, h_new, ea, eb, fluxes, dt, G, GV, tv, o
                                         G, GV, CS%ISOMIP_tracer_CSp, &
                                         evap_CFL_limit=evap_CFL_limit, &
                                         minimum_forcing_depth=minimum_forcing_depth)
+    if (CS%use_OUT_IS_tracer) &
+      call OUT_IS_tracer_column_physics(h_old, h_new, ea, eb, fluxes, dt, &
+                                        G, GV, CS%OUT_IS_tracer_CSp, &
+                                        evap_CFL_limit=evap_CFL_limit, &
+                                        minimum_forcing_depth=minimum_forcing_depth)
     if (CS%use_ideal_age) &
       call ideal_age_tracer_column_physics(h_old, h_new, ea, eb, fluxes, dt, &
                                            G, GV, CS%ideal_age_tracer_CSp, &
@@ -483,6 +502,9 @@ subroutine call_tracer_column_fns(h_old, h_new, ea, eb, fluxes, dt, G, GV, tv, o
     if (CS%use_ISOMIP_tracer) &
       call ISOMIP_tracer_column_physics(h_old, h_new, ea, eb, fluxes, dt, &
                                       G, GV, CS%ISOMIP_tracer_CSp)
+    if (CS%use_OUT_IS_tracer) &
+      call OUT_IS_tracer_column_physics(h_old, h_new, ea, eb, fluxes, dt, &
+                                      G, GV, CS%OUT_IS_tracer_CSp)
     if (CS%use_ideal_age) &
       call ideal_age_tracer_column_physics(h_old, h_new, ea, eb, fluxes, dt, &
                                            G, GV, CS%ideal_age_tracer_CSp)
@@ -710,6 +732,8 @@ subroutine call_tracer_surface_state(state, h, G, CS)
     call DOME_tracer_surface_state(state, h, G, CS%DOME_tracer_CSp)
   if (CS%use_ISOMIP_tracer) &
     call ISOMIP_tracer_surface_state(state, h, G, CS%ISOMIP_tracer_CSp)
+  if (CS%use_OUT_IS_tracer) &
+    call OUT_IS_tracer_surface_state(state, h, G, CS%OUT_IS_tracer_CSp)
   if (CS%use_ideal_age) &
     call ideal_age_tracer_surface_state(state, h, G, CS%ideal_age_tracer_CSp)
   if (CS%use_regional_dyes) &
@@ -734,6 +758,7 @@ subroutine tracer_flow_control_end(CS)
     call USER_tracer_example_end(CS%USER_tracer_example_CSp)
   if (CS%use_DOME_tracer) call DOME_tracer_end(CS%DOME_tracer_CSp)
   if (CS%use_ISOMIP_tracer) call ISOMIP_tracer_end(CS%ISOMIP_tracer_CSp)
+  if (CS%use_OUT_IS_tracer) call OUT_IS_tracer_end(CS%OUT_IS_tracer_CSp)
   if (CS%use_ideal_age) call ideal_age_example_end(CS%ideal_age_tracer_CSp)
   if (CS%use_regional_dyes) call regional_dyes_end(CS%dye_tracer_CSp)
   if (CS%use_oil) call oil_tracer_end(CS%oil_tracer_CSp)
