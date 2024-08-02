967908d518f904f1078f530fe55f0693a55c0b09
Gustavo Marques <gmarques@ucar.edu>
2024-08-02 09:04:09 -0600

    Option to apply KHTR_MIN in the whole water column
    This commit introduces a new parameter, FULL_DEPTH_KHTR_MIN, which
    enforces a user-specified minimum diffusivity (KHTR_MIN) to be
    applied throughout the entire water column, instead of only at
    the surface. This option is available only when
    KHTR_USE_EBT_STRUCT=True and KHTR_MIN > 0.

--- src/tracer/MOM_tracer_hor_diff.F90
+++ src/tracer/MOM_tracer_hor_diff.F90
@@ -54,6 +54,9 @@ type, public :: tracer_hor_diff_CS ; private
                                   !! locally at or below this value [nondim].
   logical :: KhTr_use_ebt_struct  !< If true, uses the equivalent barotropic structure
                                   !! as the vertical structure of tracer diffusivity.
+  logical :: full_depth_khtr_min  !< If true, KHTR_MIN is enforced throughout the whole water column.
+                                  !! Otherwise, KHTR_MIN is only enforced at the surface. This parameter
+                                  !! is only available when KHTR_USE_EBT_STRUCT=True and KHTR_MIN>0.
   logical :: Diffuse_ML_interior  !< If true, diffuse along isopycnals between
                                   !! the mixed layer and the interior.
   logical :: check_diffusive_CFL  !< If true, automatically iterate the diffusion
@@ -413,20 +416,39 @@ subroutine tracer_hordiff(h, dt, MEKE, VarMix, G, GV, US, CS, Reg, tv, do_online
       enddo
     enddo
     if (CS%KhTr_use_ebt_struct) then
-      do K=2,nz+1
-        do J=js-1,je
-          do i=is,ie
-            Coef_y(i,J,K) = Coef_y(i,J,1) * 0.5 * ( VarMix%ebt_struct(i,j,k-1) + VarMix%ebt_struct(i,j+1,k-1) )
+      if (CS%full_depth_khtr_min) then
+        do K=2,nz+1
+          do J=js-1,je
+            do i=is,ie
+              Coef_y(i,J,K) = Coef_y(i,J,1) * 0.5 * ( VarMix%ebt_struct(i,j,k-1) + VarMix%ebt_struct(i,j+1,k-1) )
+              Coef_y(i,J,K) = max(Coef_y(i,J,K), CS%KhTr_min)
+            enddo
           enddo
         enddo
-      enddo
-      do k=2,nz+1
-        do j=js,je
-          do I=is-1,ie
-            Coef_x(I,j,K) = Coef_x(I,j,1) * 0.5 * ( VarMix%ebt_struct(i,j,k-1) + VarMix%ebt_struct(i+1,j,k-1) )
+        do k=2,nz+1
+          do j=js,je
+            do I=is-1,ie
+              Coef_x(I,j,K) = Coef_x(I,j,1) * 0.5 * ( VarMix%ebt_struct(i,j,k-1) + VarMix%ebt_struct(i+1,j,k-1) )
+              Coef_x(I,j,K) = max(Coef_x(I,j,K), CS%KhTr_min)
+            enddo
           enddo
         enddo
-      enddo
+      else
+        do K=2,nz+1
+          do J=js-1,je
+            do i=is,ie
+              Coef_y(i,J,K) = Coef_y(i,J,1) * 0.5 * ( VarMix%ebt_struct(i,j,k-1) + VarMix%ebt_struct(i,j+1,k-1) )
+            enddo
+          enddo
+        enddo
+        do k=2,nz+1
+          do j=js,je
+            do I=is-1,ie
+              Coef_x(I,j,K) = Coef_x(I,j,1) * 0.5 * ( VarMix%ebt_struct(i,j,k-1) + VarMix%ebt_struct(i+1,j,k-1) )
+            enddo
+          enddo
+        enddo
+      endif
     endif
 
     do itt=1,num_itts
@@ -1573,6 +1595,13 @@ subroutine tracer_hor_diff_init(Time, G, GV, US, param_file, diag, EOS, diabatic
   call get_param(param_file, mdl, "KHTR_MIN", CS%KhTr_Min, &
                  "The minimum along-isopycnal tracer diffusivity.", &
                  units="m2 s-1", default=0.0, scale=US%m_to_L**2*US%T_to_s)
+  if (CS%KhTr_use_ebt_struct .and. CS%KhTr_Min > 0.0) then
+    call get_param(param_file, mdl, "FULL_DEPTH_KHTR_MIN", CS%full_depth_khtr_min, &
+                   "If true, KHTR_MIN is enforced throughout the whole water column. "//&
+                   "Otherwise, KHTR_MIN is only enforced at the surface. This parameter "//&
+                   "is only available when KHTR_USE_EBT_STRUCT=True and KHTR_MIN>0.",      &
+                   default=.false.)
+  endif
   call get_param(param_file, mdl, "KHTR_MAX", CS%KhTr_Max, &
                  "The maximum along-isopycnal tracer diffusivity.", &
                  units="m2 s-1", default=0.0, scale=US%m_to_L**2*US%T_to_s)
